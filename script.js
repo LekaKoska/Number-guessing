780ecfffadcaca0481b3eef01dac5a1b757630f6
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-01-19 14:36:56 +0100

    * Creating basic logic for this task

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const min = 1;
 const max = 100;
 let answer = Math.floor(Math.random() * (max - min) + 1);
+let attempts = 0;
+let userGuess;
+let winner = false;
+do {
+	userGuess = Number(prompt("Enter a number"));
+	if (attempts >= 3) {
+		alert("You used maximum limit please restart game to get new chance");
+		break;
+	}
+	if (isNaN(userGuess)) {
+		alert("Your guess is not a number");
+	}
+
+	if (userGuess === answer) {
+		winner = true;
+		break;
+	}
+	attempts++;
+} while (!winner);
+
+console.log("You win");
