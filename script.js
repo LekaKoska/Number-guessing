def4fbedb633fba0c5a803669cd407c3d0467387
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-01-19 14:57:37 +0100

    * Implementing logic for guessing number's between 1 and 50 * Adding let attempts

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const min = 1;
-const max = 100;
+const max = 50;
 let answer = Math.floor(Math.random() * (max - min) + 1);
 let attempts = 0;
 let userGuess;
 let winner = false;
 do {
-	userGuess = Number(prompt("Enter a number"));
-	if (attempts >= 3) {
+	console.log(answer);
+	userGuess = Number(prompt(`Guess a number between ${min} and ${max}`));
+	if (attempts >= 4) {
 		alert("You used maximum limit please restart game to get new chance");
 		break;
 	}
 	if (isNaN(userGuess)) {
-		alert("Your guess is not a number");
+		window.alert("Your guess is not a number");
+	} else if (userGuess < min || userGuess > max) {
+		window.alert(`Please enter a valid number between ${min} and ${max}`);
+	} else {
+		if (userGuess < answer) {
+			window.alert("To low, try again!");
+		} else if (userGuess > answer) {
+			window.alert("To high, try again");
+		}
+		attempts++;
 	}
-
 	if (userGuess === answer) {
 		winner = true;
+		window.alert(`CONGRATULATIONS!! Its took ${attempts} attemps`);
 		break;
 	}
-	attempts++;
 } while (!winner);
-
-console.log("You win");
